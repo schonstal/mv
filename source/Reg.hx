94336bd6a8a879d022eea00cfc92b37434630f05
Josh Schonstal <josh@schonstal.org>
2015-04-18 09:59:32 -0700

    Iinitial commit

---
+++ source/Reg.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.util.FlxSave;
+
+/**
+ * Handy, pre-built Registry class that can be used to store 
+ * references to objects and other things for quick-access. Feel
+ * free to simply ignore it or change it in any way you like.
+ */
+class Reg
+{
+  public static var level:Int = 1;
+
+  // Which color palette to use
+  public static var palette:Int = 1;
+}
